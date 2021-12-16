7060d2de1856b37fbb1d35c68d69c4bdb0042e1e
Jacob GI <jacob.demmane@gmail.com>
2021-12-16 12:21:17 +0300

    preparing the body to resize when used in iFrame

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./style/index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import UpdateCRO from "./admin/updateCRO";
+// import UpdateCRO from "./admin/updateCRO";
 
 import Header from "./components/Header";
 
@@ -9,32 +9,32 @@ import Content from "./components/Content";
 
 import checklists from "./data";
 
-function App() {
-  return <UpdateCRO checklists={checklists} />;
-}
 // function App() {
-//   return (
-//     <Router>
-//       <div className='App'>
-//         <Header />
-//         <Routes>
-//           <Route path='/' element={<Content checklist={checklists[0]} />} />
-//           <Route
-//             path='/home-pages'
-//             element={<Content checklist={checklists[1]} />}
-//           />
-//           <Route
-//             path='/product-pages'
-//             element={<Content checklist={checklists[2]} />}
-//           />
-//           <Route
-//             path='/cart-pages'
-//             element={<Content checklist={checklists[3]} />}
-//           />
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
+//   return <UpdateCRO checklists={checklists} />;
 // }
+function App() {
+  return (
+    <Router>
+      <div className='App'>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Content checklist={checklists[0]} />} />
+          <Route
+            path='/home-pages'
+            element={<Content checklist={checklists[1]} />}
+          />
+          <Route
+            path='/product-pages'
+            element={<Content checklist={checklists[2]} />}
+          />
+          <Route
+            path='/cart-pages'
+            element={<Content checklist={checklists[3]} />}
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
 
 export default App;
